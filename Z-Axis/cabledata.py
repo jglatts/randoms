ea13f966fcc88f8facf341d6a0c773ed3b1f8a62
John Glatts <john@z-axiscc.com>
2020-05-25 11:49:09 -0400

    Update cabledata.py

--- Z-Axis/cabledata.py
+++ Z-Axis/cabledata.py
@@ -1,28 +1,32 @@
 import os
+from decimal import *
 
 class CableData(object):
 
-    # use this to check all the data and find an average pass/fail rate
+    def __init__(self):
+        self.count = 0
+        self.totalErrors = 0
+        self.totalPasses = 0
+        self.verbose = False
+
     def readAllFiles(self):
         files = [f for f in os.listdir('.') if os.path.isfile(f) and f[-1] != "y"]
-        count = 1
         for f in files:
-           self.readFile(f, count)
-           count += 1
+           self.readFile(f)
+           self.count += 1
 
-    # add data processing stuff
-    def readFile(self, filename, count):
+    def readFile(self, filename):
         f = open(filename, "r")
     
         #read off some data
         for i in range(7):
             f.readline()   
     
-        self.readTestData(f, count)         
+        self.readTestData(f)         
         f.close()
 
-    def readTestData(self, f, count):
-        #read critical data
+    def readTestData(self, f):
+        passed = True
         c = f.readline().split()
         p = f.readline().split("-")
         check = c[len(c)-1]
@@ -32,14 +36,35 @@ class CableData(object):
         #print data for each pin
         for x in range(pins):
            s = f.readline().split()
-           z = s[0]
            if (s[2] == check and check == "OL"):
                pincount += 1
-               z = z[1:2]
-        print("Data File: " + str(count) + " Testing " + str(pins) + " pins, Errors: " + str(pincount))       
+               passed = False
     
+        if (passed):
+            self.totalPasses += 1
+        else:
+            self.totalErrors += 1
+        if (self.verbose):
+            print("Testing: " + f.name + " Data File: " + str(self.count+1) + " Testing " + str(pins) + " pins, Errors: " + str(pincount) + "\n")       
+        else:
+            print("Data File: " + str(self.count+1) + " Testing " + str(pins) + " pins, Errors: " + str(pincount))       
     
+    def printResults(self):
+        print("\nTested: " + str(self.count) + " cables")
+        print("Total Passes: " + str(self.totalPasses))
+        print("Total Errors: " + str(self.totalErrors))
+        sum = self.totalPasses + self.totalErrors
+        print("%pass " + str(round((self.totalPasses / sum) * 100, 2)) + "%")
+        print("%error " + str(round((self.totalErrors / sum) * 100, 2)) + "%")
+
     def testData(self):
         self.readAllFiles()
 
+    def testData(self):
+        self.readAllFiles()
 
+    def testDataVerbose(self):
+        print("\n-------------------Verbose Output Enabled-------------------\n")
+        self.verbose = True
+        self.readAllFiles()
+        self.verbose = False
