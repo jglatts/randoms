68b94c8c3c57397d2a1ee8413436dda361b296e4
John Glatts <john@z-axiscc.com>
2020-05-28 22:42:47 -0400

    Update cabledata.py

--- Z-Axis/cabledata.py
+++ Z-Axis/cabledata.py
@@ -1,9 +1,6 @@
 import os
 from decimal import *
 
-# thrown in a str.ljust() 
-# to format string 
-
 class CableData(object):
 
     def __init__(self):
@@ -49,53 +46,63 @@ class CableData(object):
         check = c[len(c)-1]
         limcheck = h[len(h)-1]  #add another helper() to check for OL lims
         pins = int(p[len(p)-1])
-        pincount = 0
+        errcount = 0
 
         #print data for each pin
         for x in range(pins):
            s = f.readline().split()
-           if (s[2] == check and self.fixtureFailed(check, limcheck)):
-               pincount += 1
+           if (s[2] == check and check == "OL"):
+               errcount += 1
                self.totalPinErrors += 1
                passed = False   
            else:
                self.totalPinPasses += 1
         
-        self.checkData(passed, f, pins, pincount)  
+        self.checkData(passed, f, pins, errcount)  
         print("Resistance Actual Value = " + check)
         print("Resistance High Limit   = " + limcheck)    
 
-    
-    def fixtureFailed(self, check, highlimit):
-        out = False
-
-        if (check == "OL"):
-            self.totalOLErrors += 1
-            out = True
-        elif (check > highlimit):
-            out = True
-        
-        return out
 
 
-    def checkData(self, passed, f, pins, pincount):
+    def checkData(self, passed, f, pins, errcount):
         if (passed):
             self.totalPasses += 1
         else:
             self.totalErrors += 1
         if (self.verbose):
-            print("Testing: " + f.name + " Data File: " + str(self.count+1) + " Testing " + str(pins) + " pins, Errors: " + str(pincount))       
+            print("Testing: " + f.name + " Data File: " + str(self.count+1) + " Testing " + str(pins) + " pins, Errors: " + str(errcount))       
             print("Raw File = ")
             print(self.copyoutput + "\n")
         else:
-            print("Data File #" + str(self.count+1) + ", Testing " + str(pins) + " pins, Errors: " + str(pincount))  
+            print("Data File #" + str(self.count+1) + ", Testing " + str(pins) + " pins, Errors: " + str(errcount))  
+
+
+    def checkForOpens(self):
+        count = 0
+        files = [f for f in os.listdir('.') if self.isTextFile(f)]
+        for f in files:
+            isopen = False
+            file = open(f, "r")
+            line = file.readline()
+            while (line):
+                str = line.split()
+                for s in str:
+                    if (s == "OL"):
+                        isopen = True
+                        break
+                line = file.readline()
+            file.close()
+            if (isopen):
+                count += 1
+
+        return count
 
 
     def printResults(self):
         print("\nTested: " + str(self.count) + " cables")
         print("Total Passes: " + str(self.totalPasses))
         print("Total Fails: " + str(self.totalErrors))
-        print("Total OL Fails: " + str(self.totalOLErrors)) # misleading change this
+        print("Total OL Fails: " + str(self.checkForOpens()))
         print("Total Pin Passes: " + str(self.totalPinPasses))
         print("Total Pin Errors: " + str(self.totalPinErrors))        
         sum = self.totalPasses + self.totalErrors
@@ -119,6 +126,7 @@ class CableData(object):
 
     def testData(self):
         self.readAllFiles()
+
 
 
     def testDataVerbose(self):
