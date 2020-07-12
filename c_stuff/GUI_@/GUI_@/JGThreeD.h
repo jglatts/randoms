5461b46fd6f6a0637d2a5502264b411459f9caff
John Glatts <john@z-axiscc.com>
2020-07-12 10:52:42 -0400

    clean up

--- c_stuff/GUI_@/GUI_@/JGThreeD.h
+++ c_stuff/GUI_@/GUI_@/JGThreeD.h
@@ -5,15 +5,11 @@
 
 class TCEExample : public olc::PixelGameEngine
 {
+
+public:
 	tce::Renderer renderer = tce::Renderer(this);
 	tce::Renderer renderer_two = tce::Renderer(this);
 	float z = 0;
-
-private:
-	int cam_position_x = 0;
-	int cam_position_y = 0;
-
-public:
 
 	TCEExample()
 	{
@@ -21,7 +17,6 @@ public:
 		renderer.options.renderDistance[0] = 1; // Set the near render distance to 5
 	}
 
-public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
@@ -31,15 +26,37 @@ public:
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		Clear(olc::BLACK);
+		DrawHeaderString();
+		SetCamPosition();
+		DrawFaces();
+		return true;
+	}
+
+private:
+	int cam_position_x = 0;
+	int cam_position_y = 0;
+
+	void DrawHeaderString()
+	{
+		// get these guys to move xD
 		olc::PixelGameEngine::DrawString(20, 45, "JDG 3D", olc::DARK_CYAN, 3);
+		olc::PixelGameEngine::DrawString(45, 85, "2020", olc::DARK_CYAN, 3);
+	}
+
+	void SetCamPosition() 
+	{
 		if (cam_position_y > 120) cam_position_y = 0;
 		renderer.camera.position.y = cam_position_y;
 		renderer_two.camera.position.y = cam_position_y;
 		cam_position_y += 3;
+	}
+
+	void DrawFaces()
+	{
 		tce::DrawCube(tce::Vec3D(-11, 0, 25), tce::Vec3D(50, 50, 50), &renderer);
 		tce::DrawCube(tce::Vec3D(-11, 60, 25), tce::Vec3D(10, 10, 10), &renderer_two);
 		renderer.render();
 		renderer_two.render(olc::DARK_CYAN);
-		return true;
 	}
+	
 };
